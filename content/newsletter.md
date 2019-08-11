2aa76082411cb1b20247972a8e23d073c73452a1
Pascal Precht <pascal.precht@gmail.com>
2019-08-11 16:07:22 +0200

    Add newsletter page

---
+++ content/newsletter.md
@@ -0,0 +1,5 @@
+---
+title: "Newsletter"
+template: "newsletter"
+---
+
--- gatsby/create-pages.js
+++ gatsby/create-pages.js
@@ -62,6 +62,12 @@ const createPages = async ({ graphql, actions }) => {
         component: path.resolve('./src/templates/post-template.js'),
         context: { slug: edge.node.fields.slug }
       });
+    } else if (_.get(edge, 'node.frontmatter.template') === 'newsletter') {
+      createPage({
+        path: edge.node.fields.slug,
+        component: path.resolve('./src/templates/newsletter-template.js'),
+        context: { slug: edge.node.fields.slug }
+      });
     }
   });
 
